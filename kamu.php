b7e1cf3e78e09718cbc9f4db055330f1c9808639
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2022-05-24 15:47:38 +0700

    Add files via upload

--- kamu.php
+++ kamu.php
@@ -2,10 +2,10 @@
 date_default_timezone_set('Asia/Jakarta');
 include "function.php";
 echo color("green","# # # # # # # # # # # # # # # # # # # # # # # \n");
-echo color("yellow","[•]      Wellcome Para Colikiawan  \n");
-echo color("yellow","[•]  Time  : ".date('[d-m-Y] [H:i:s]')."   \n");
-echo color("yellow","[•]        Waiting process.....           \n");
-echo color("yellow","[•] Cara menulis nomor pakai 62xxxxxxxxxx \n");
+echo color("yellow","[•]       Wellcome Para Cendikiawan        [•]\n");
+echo color("yellow","[•]    Time  : ".date('[d-m-Y] [H:i:s]')."     [•]\n");
+echo color("yellow","[•]           Waiting process.....         [•] \n");
+echo color("yellow","[•] Cara menulis nomor pakai 62xxxxxxxxxx  [•]\n");
 echo color("green","# # # # # # # # # # # # # # # # # # # # # # # \n");
 function change(){
         $nama = nama();
